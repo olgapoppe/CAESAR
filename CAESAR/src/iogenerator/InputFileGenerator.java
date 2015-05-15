999a5fd15055deb76c8f3012d4c2afbd79dca3bc
olgapoppe <olga.poppe@gmail.com>
2015-05-15 10:43:52 -0400

    Added methods to input file generator to select and alter tuples

--- CAESAR/src/iogenerator/InputFileGenerator.java
+++ CAESAR/src/iogenerator/InputFileGenerator.java
@@ -17,36 +17,95 @@ public class InputFileGenerator {
 	
 	public static void main (String[] args) {
 	
-		Scanner s = null;
+		Scanner input = null;
 		try {		
 			/*** Input file ***/
-			//File f = new File("src/input/datafile20seconds.dat");
-			File f = new File("../../Dropbox/LR/InAndOutput/1xway/input7.dat");
-			s = new Scanner(f);  			
+			//File input_file = new File("src/input/datafile20seconds.dat");
+			File input_file = new File("../../Dropbox/LR/InAndOutput/1xway/input7.dat");
+			input = new Scanner(input_file);  			
 					
 			/*** Output file ***/
-            File input_file = new File("../../input.dat");
-            BufferedWriter position_reports = new BufferedWriter(new FileWriter(input_file));
+            File output_file = new File("../../input.dat");
+            BufferedWriter output = new BufferedWriter(new FileWriter(output_file));
             
-            String eventString = "";
-            int count = 0;
-            //int number_of_position_reports = 357853; // Integer.parseInt(args[0]);
+            // Integer.parseInt(args[0]);
+            // 357853 events are within the first for 1500 seconds 
             
-            /*** Write output file ***/
-            while (s.hasNextLine()) { // && count<number_of_position_reports) {        	
-            	count++;
-            	eventString = s.nextLine();
-            	PositionReport event = PositionReport.parse(eventString);
-            	if (event.type == 0 && event.dir == 0) position_reports.write(eventString + "\n");            	            	            	         	
-            }              
+            /*** Call method ***/
             
-            // Print out event number and last event
-            System.out.println("Count: " + count + " Last event: " + eventString);
+            
             
             /*** Clean-up ***/
-       		s.close();
-       		position_reports.close();        		
+       		input.close();
+       		output.close();        		
         
 		} catch (IOException e) { System.err.println(e); }		  
+	}
+	
+	
+	/**
+	 * Copy position reports from input to output and change their xway to the given value 
+	 * @param input
+	 * @param output
+	 * @param new xway
+	 */
+	public void changeXway (Scanner input, BufferedWriter output, int newXway) {
+		
+		
+	}
+	
+	/**
+	 * Copy position reports that have the given direction from input to output
+	 * @param input
+	 * @param output
+	 * @param dir
+	 */
+	public void selectTuples (Scanner input, BufferedWriter output, int dir) {
+		
+		String eventString = "";		
+		try {
+			while (input.hasNextLine()) {         	
+        			
+				eventString = input.nextLine();
+				PositionReport event = PositionReport.parse(eventString);
+				if (event.type == 0 && event.dir == 0) output.write(eventString + "\n");            	            	            	         	
+			}   
+		} catch (IOException e) { System.err.println(e); }	
+	}
+	
+	/**
+	 * Copy the given number of tuples from input to output
+	 * @param input
+	 * @param output
+	 * @param tuple number
+	 */
+	public void copyTuples (Scanner input, BufferedWriter output, int tupleNumber) {
+		
+		String eventString = "";
+		int count = 0; 
+		try {
+			while (input.hasNextLine() && count < tupleNumber) {         	
+        		
+				count++;
+				eventString = input.nextLine();
+				output.write(eventString + "\n");            	            	            	         	
+			}   
+		} catch (IOException e) { System.err.println(e); }				
+	}
+	
+	/**
+	 * Count the number of tuples in the input and print it and the last tuple
+	 * @param input 
+	 */
+	public void countTuples (Scanner input) {
+		
+		String eventString = "";	
+		int count = 0; 
+		while (input.hasNextLine()) {         	
+        			
+			count++;
+			eventString = input.nextLine();
+		} 
+		System.out.println("Count: " + count + " Last event: " + eventString);		
 	}
 }
