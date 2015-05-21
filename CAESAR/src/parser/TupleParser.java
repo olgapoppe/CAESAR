7d59fdcb4fbda590c7fa408d8153211edc70d9d8
opoppe <olga.poppe@gmail.com>
2015-05-21 09:28:51 -0400

    Parser-based solution works for 9000 seconds of tuples from 6 highways

--- CAESAR/src/parser/TupleParser.java
+++ CAESAR/src/parser/TupleParser.java
@@ -20,7 +20,8 @@ public class TupleParser {
 			
 			// Time
 			Integer sec = 0;	
-			//int count = 0;
+			int count = 0;
+			int output_sec = 0;
 			
 			// First event
 			String line = scanner.nextLine();
@@ -29,13 +30,13 @@ public class TupleParser {
 			while (sec <= lastSec) {
 				
 				ArrayList<PositionReport> events = new ArrayList<PositionReport>();
-				//count = 0;
+				count = 0;
 				
 				// Put events with time stamp sec into the list and reset the input event	 		
 		 		while (event != null && event.sec == sec) {		 			
 		 					 			
 		 			events.add(event);	
-		 			//count++;
+		 			count++;
 		 					 			
 		 			// Reset event
 		 			if (scanner.hasNextLine()) {		 				
@@ -45,8 +46,11 @@ public class TupleParser {
 		 				event = null;		 				
 		 			}
 		 		}
-		 		all_events.put(sec, events);		 		
-		 		//System.out.println(count + " events during second " + sec + " were parsed.");		 		
+		 		all_events.put(sec, events);	
+		 		if (sec == output_sec + 300) { 
+		 			System.out.println(count + " events were parsed during second " + sec);		
+		 			output_sec += 300;
+		 		}
 		 		sec++;
 			}			
 			/*** Clean-up ***/		
