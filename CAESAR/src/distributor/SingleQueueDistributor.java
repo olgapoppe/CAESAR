7cda57630a970a7e1c7f3db97f8f1e4aa1dd5cbe
olgapoppe <olga.poppe@gmail.com>
2015-06-14 18:34:41 -0400

    Chasing a bug

--- CAESAR/src/distributor/SingleQueueDistributor.java
+++ CAESAR/src/distributor/SingleQueueDistributor.java
@@ -63,7 +63,7 @@ public class SingleQueueDistributor extends EventDistributor {
 			 				
 			 				curr_ms = System.currentTimeMillis() - startOfSimulation;
 			 				distributorProgressPerSec.put(prev_sec, curr_ms);		 		
-			 				runqueues.setDistributorProgress(prev_sec);		 				
+			 				//runqueues.setDistributorProgress(prev_sec);		 				
 			 				//System.out.println("Distr progr:" + prev_sec + " Distr ms: " + curr_ms);
 			 				
 			 				prev_sec++;
