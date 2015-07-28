0bf72e00f5f9f819c1f726e2a33c3fad82c1a2c8
olgapoppe <olga.poppe@gmail.com>
2015-07-28 14:03:53 -0400

    Distributor progress is not set for the first 10 seconds

--- CAESAR/src/distributor/SingleQueueDistributor.java
+++ CAESAR/src/distributor/SingleQueueDistributor.java
@@ -74,7 +74,7 @@ public class SingleQueueDistributor extends EventDistributor {
 		 		/*** Set distributor progress per second ***/
 		 		if (prev_sec < event.sec) {
 		 				
-		 			eventqueues.setDistributorProgress(prev_sec);					
+		 			if (prev_sec > 10) eventqueues.setDistributorProgress(prev_sec);					
 					
 					//System.out.println("Distributor progress for " + prev_sec + " is " + now);
 					distributorProgressPerSec.put(prev_sec,now);
