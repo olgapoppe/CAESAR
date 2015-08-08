9e4b614c85387c682ec80fde1a71bd61850ed6e9
olgapoppe <olga.poppe@gmail.com>
2015-08-07 22:42:43 -0400

    Operator omission

--- CAESAR/src/optimizer/Optimizer.java
+++ CAESAR/src/optimizer/Optimizer.java
@@ -1,24 +1,29 @@
 package optimizer;
 
-import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collections;
-import java.util.List;
+import java.util.LinkedList;
 import operator.*;
 
 public class Optimizer {
 	
-	static void omit (List<Operator> query_plan, int k) {
+	static void omit (LinkedList<Operator> query_plan) {
 		
-		for (Operator operator : query_plan) {
-			// if (operator.omittable()) {
-				 
-			 //}
+		for (int i=0; i<query_plan.size(); i++) {
+			
+			Operator operator = query_plan.get(i);
+			Operator before = query_plan.get(i-1);
+			Operator after = query_plan.get(i+1);
+			if (operator.omittable(before) || operator.omittable(after)) {
+				 LinkedList<Operator> new_query_plan = new LinkedList<Operator>();
+				 new_query_plan.addAll(query_plan);
+				 new_query_plan.remove(i);
+			}
 		}
 		
 	}
 	
-	static void permute (List<Operator> arr, int k) {
+	static void permute (LinkedList<Operator> arr, int k) {
         for(int i = k; i < arr.size(); i++){
             Collections.swap(arr, i, k);
             permute(arr, k+1);
@@ -30,7 +35,7 @@ public class Optimizer {
     }
     public static void main(String[] args) {
     	
-    	ArrayList<Operator> query_plan = new ArrayList<Operator>();
+    	LinkedList<Operator> query_plan = new LinkedList<Operator>();
     	
     	
     	
